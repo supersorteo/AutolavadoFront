c2e0b4dc89d7418ef0f48227f6e642e42ff4cdf1
supersorteo <soporte.supersorteo@gmail.com>
2026-02-15 20:39:11 -0500

    cambios para danilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,11 @@ export class AppComponent {
   showPassword = false;
   isCheckingAuth = true;
   //private apiUrl = "http://localhost:8080";
-  private apiUrl = "https://excellsiorback-production.up.railway.app"
+  //private apiUrl = "https://excellsiorback-production.up.railway.app"
+
+
+  //danilo pruebas
+  private apiUrl = "https://exellssiorpruebadanilo1-production.up.railway.app"
   token = '';
 
   constructor(private http: HttpClient) {
